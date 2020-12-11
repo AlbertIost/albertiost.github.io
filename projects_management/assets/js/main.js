651a5af8dd8a369adae17c5304859be7bd7fd0c1
Albert Iost <albertiost@mail.ru>
2020-12-12 00:40:39 +0700

    projects slider header

---
+++ projects_management/assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.menu-btn').click(function(e){
+		e.preventDefault;
+		$(this).toggleClass('menu-btn_active');
+		$(this).next('.menu').slideToggle(300);
+	});
+	$('.login-auth .mini-avatar').click(function(){
+		$(this).next('.actions').slideToggle(300);
+	});
+});
