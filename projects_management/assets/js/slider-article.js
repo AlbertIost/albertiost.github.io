75059e014cfe8750ef42803e9cccbedcbb63837a
Albert Iost <albertiost@mail.ru>
2020-12-15 23:43:33 +0700

    slider infopage

---
+++ projects_management/assets/js/slider-article.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	$('.slider-research').slick({
+		dots: true,
+		arrows: false,
+		appendDots: '.slider-research__dots',
+		responsive:[{
+			breakpoint: 5000,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+			}
+		},
+		{
+			breakpoint: 1651,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+			}
+		},
+		{
+			breakpoint: 1001,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+		},
+		{
+			breakpoint: 801,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			}
+		},
+		{
+			breakpoint: 601,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		}]
+	});
+	$('.rec-section').slick({
+		responsive:[{
+			breakpoint: 5000,
+			settings: 'unslick'
+		},
+		{
+			breakpoint: 725,
+			settings: {
+				slidesToShow: 1,
+				arrows: true
+			}
+		}]
+	});
+});
